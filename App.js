fca8312515d3ca2b11f7d3e5e66701ff9c4015d1
Jacob Sanford <jacob.r.sanford@gmail.com>
2023-02-22 17:51:03 -0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native'
+import 'react-native-gesture-handler';
+
+function ChicagoScreen(){
+  return(
+    <View>
+      <Image style={styles.image} source={require('./assets/art.png')} />
+    </View>
+  )
+};
+
+function WillisScreen(){
+  return(
+    <View>
+      <Image style={styles.image} source={require('./assets/willis.png')} />
+    </View>
+  )
+};
+
+function MagnificentScreen(){
+  return(
+    <View>
+      <Image style={styles.image} source={require('./assets/mile.png')} />
+    </View>
+  )
+};
+
+function NavyScreen(){
+  return(
+    <View>
+      <Image style={styles.image} source={require('./assets/pier.png')} />
+    </View>
+  )
+};
+
+function WaterScreen(){
+  return(
+    <View>
+      <Image style={styles.image} source={require('./assets/water.png')}/>
+    </View>
+  )
+}
+
+
+
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name='Art Institute of Chicago' component={ChicagoScreen}></Drawer.Screen>
+        <Drawer.Screen name='Magnificent Mile' component={MagnificentScreen}></Drawer.Screen>
+        <Drawer.Screen name='Navy Pier' component={NavyScreen}></Drawer.Screen>
+        <Drawer.Screen name='Water Tower' component={WaterScreen}></Drawer.Screen>
+        <Drawer.Screen name='Willis Tower' component={WillisScreen}></Drawer.Screen>
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +70,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  image:{
+    width: 320,
+    height: 480,
+    alignSelf: 'center',
+    bottom: '-20%'
+  }
 });
